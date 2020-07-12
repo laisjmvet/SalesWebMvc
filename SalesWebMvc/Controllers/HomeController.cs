5d12774207f178ccba782b1906af674ff3680903
Lais Moraes <laisjmvet@gmail.com>
2020-07-12 11:00:05 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from a C# Course";
+            ViewData["Student"] = "Lais Moraes";            
 
             return View();
         }
